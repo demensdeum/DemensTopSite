3b979ebd0eb34e9af2fae4e02b4ccaf29abdb75b
DEMENSDEUM-ACER\Demensdeum <demensdeum@gmail.com>
2023-11-10 20:35:26 +0300

    Changes

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace DemensTopSite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var lastSavedAddress = Properties.Settings.Default.LastSiteAddress;
+            handleAddress(lastSavedAddress);
+        }
+
+        private void showAlert(string title, string message)
+        {
+            MessageBox.Show(message, title);
+        }
+
+        private void handleAddress(string address)
+        {
+            addressTextBox.Text = address;
+            if (address.StartsWith("https://") == false)
+            {
+                address = "https://" + address;
+            }
+            try
+            {
+                webView.Source = new Uri(address);
+                Properties.Settings.Default.LastSiteAddress = address;
+                Properties.Settings.Default.Save();
+            }
+            catch (Exception exception)
+            {
+                showAlert("Error!", $"{exception.Message}: {address}");
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var address = addressTextBox.Text;
+                handleAddress(address);
+            }
+        }
+    }
+}
